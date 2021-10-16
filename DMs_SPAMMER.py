ba9fc1b690debb4c9c4a14fd60540a287861d5da
karma.meme <86238378+hoemotion@users.noreply.github.com>
2021-10-16 18:51:47 +0200

    Update DMs_SPAMMER.py

--- DMs_SPAMMER.py
+++ DMs_SPAMMER.py
@@ -1,82 +1,90 @@
-import sys
-import subprocess
-try:
-    import discord
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", 'discord.py'])
-    subprocess.check_call([sys.executable, "-m", "pip", "install", 'discord'])
+import sys, subprocess, random, json, asyncio, os
 from discord.ext import commands
-import json
 try:
-    import colorama
+    import colorama, pyfade, discord
 except ImportError:
     subprocess.check_call([sys.executable, "-m", "pip", "install", 'colorama'])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", 'pyfade'])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", 'discord.py'])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", 'discord'])
 from colorama import Fore
 from datetime import datetime
-import asyncio
-import time
-try:
-    import pyfade
-except ImportError:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", 'pyfade'])
 
 sys.tracebacklimit = 0
 bot = discord.Client()
+with open('config.json') as f:
+    yamete_kudasai = json.load(f)
+token = yamete_kudasai['token']
+cooldown = yamete_kudasai['cooldown']
+cooldown_max = cooldown + 10
+message = yamete_kudasai['message']
+duplicate = yamete_kudasai['dm_already_dmed_users']
+if duplicate == "True":
+    munanyo = "True"
+elif duplicate == "False":
+    munanyo = "False"
 
-token = input(pyfade.Fade.Horizontal(pyfade.Colors.col, f"Please enter your token>> "))
-cooldown = int(input(f"{Fore.LIGHTWHITE_EX}Enter your cooldown (time in seconds)>> "))
-message = input(f"{Fore.LIGHTWHITE_EX}What Should I Send?>> ")
 
 print(pyfade.Fade.Horizontal(pyfade.Colors.blue_to_cyan, '''
-
 ███╗   ███╗ █████╗  ██████╗ ██████╗        ██████╗ ███╗   ███╗
 ████╗ ████║██╔══██╗██╔════╝██╔════╝        ██╔══██╗████╗ ████║
 ██╔████╔██║███████║╚█████╗ ╚█████╗         ██║  ██║██╔████╔██║
 ██║╚██╔╝██║██╔══██║ ╚═══██╗ ╚═══██╗        ██║  ██║██║╚██╔╝██║
 ██║ ╚═╝ ██║██║  ██║██████╔╝██████╔╝        ██████╔╝██║ ╚═╝ ██║
-╚═╝     ╚═╝╚═╝  ╚═╝╚═════╝ ╚═════╝         ╚═════╝ ╚═╝     ╚═╝
-'''))
-print(f'''
-{Fore.LIGHTWHITE_EX}                                             Made by {Fore.YELLOW}hoemotion 
+╚═╝     ╚═╝╚═╝  ╚═╝╚═════╝ ╚═════╝         ╚═════╝ ╚═╝     ╚═╝'''))
+print(f'''{Fore.LIGHTWHITE_EX}                                             Made by {Fore.YELLOW}hoemotion 
 {Fore.LIGHTWHITE_EX}Check out the github page for updates: {Fore.LIGHTBLUE_EX}https://github.com/hoemotion/mass-dm-discord/  
 ''')
-
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.idle)
     print(f'{Fore.LIGHTGREEN_EX}Logged in as: {Fore.YELLOW}"{bot.user}" {Fore.LIGHTGREEN_EX}| ID: {Fore.YELLOW}"{bot.user.id}"{Fore.LIGHTGREEN_EX}\nConnected with {Fore.YELLOW}{len(bot.guilds)}{Fore.LIGHTGREEN_EX} Guilds and {Fore.YELLOW}{len(bot.user.friends)} {Fore.LIGHTGREEN_EX}Friends')
     print(f'{Fore.LIGHTYELLOW_EX}[⚡] Started sending DMs to the IDs\n')
-    now = datetime.now()
-    current_time = now.strftime("%H:%M")
 
     with open("ids.json", "r") as file:
         data = json.load(file)
     with open("blacklistedids.json", "r") as file:
         blcklstdata = json.load(file)
+    with open("alreadyusedids.json", "r") as file:
+        penis = json.load(file)
 
 
     indx = 0
     for i in data:
+        now = datetime.now()
+        current_time = now.strftime("%H:%M")
         indx += 1
         chupapi = await bot.fetch_user(i)
         if chupapi.id in blcklstdata:
             print(f"{Fore.BLUE}{current_time} {Fore.BLACK}[x] Blacklisted User {Fore.YELLOW}{chupapi} {Fore.BLACK}{indx} / {len(data)}")
             await asyncio.sleep(2)
+        elif munanyo == "False":
+            if chupapi.id in penis:
+                print(f"{Fore.BLUE}{current_time} {Fore.LIGHTMAGENTA_EX}[x] Avoiding Duplicates: {Fore.YELLOW}{chupapi} {Fore.BLACK}{indx} / {len(data)}")
+                await asyncio.sleep(2)
         else:
             try:
                 await chupapi.send(message)
                 print(f"{Fore.BLUE}{current_time} {Fore.LIGHTGREEN_EX}[+] Sent {message} to {Fore.YELLOW}{chupapi}{Fore.LIGHTGREEN_EX} {indx} / {len(data)}")
-                await asyncio.sleep(cooldown)
+                await asyncio.sleep(random.randint(cooldown, cooldown_max))
             except discord.Forbidden as e:
                 if e.code == 40003:
-                    print(f"{Fore.LIGHTYELLOW_EX}You have been Rate Limited\nMass DM will be continued in 2 minutes - {Fore.RED}{e}")
-                    await asyncio.sleep(120)
+                    print(f"{Fore.LIGHTYELLOW_EX}You have been Rate Limited\nThe Code will be restarted in 45 seconds - {Fore.RED}{e}")
+                    await asyncio.sleep(45)
+                    os.execv(sys.executable, ['python'] + sys.argv)
                     continue
                 else:
                     print(f"{Fore.BLUE}{current_time} {Fore.RED}[-] Couldn\'t send a DM to {Fore.YELLOW}{chupapi}{Fore.RED} - {e} {indx} / {len(data)}")
-                    await asyncio.sleep(cooldown)
+                    await asyncio.sleep(random.randint(cooldown, cooldown_max))
+            if chupapi.id not in penis:
+                await asyncio.sleep(0.01)
+                penis.append(chupapi.id)
 
-    input("Press Enter 5 times to close the program.")
+                with open("alreadyusedids.json", "w") as file:
+                    await asyncio.sleep(0.01)
+                    json.dump(penis, file)
+
+    input(f"{Fore.LIGHTGREEN_EX}Press Enter 5 times to close the program.")
     [input(i) for i in range(4, 0, -1)]
     raise SystemExit
 bot.run(token, bot = False)
