b4a72b63138f60392f212ff979c70ae521f54ecf
karma.meme <86238378+hoemotion@users.noreply.github.com>
2021-10-23 18:06:44 +0200

    Update DMs_SPAMMER.py

--- Mass-DM/DMs_SPAMMER.py
+++ Mass-DM/DMs_SPAMMER.py
@@ -50,12 +50,12 @@ async def mass_dm():
         data = json.load(file)
     with open("blacklistedids.json", "r") as file:
         blcklstdata = json.load(file)
-    with open("alreadyusedids.json", "r") as file:
-        penis = json.load(file)
 
 
     indx = 0
     for i in data:
+        with open("alreadyusedids.json", "r") as file:
+            penis = json.load(file)
         now = datetime.now()
         current_time = now.strftime("%H:%M")
         indx += 1
@@ -158,11 +158,11 @@ async def mass_dm_embed():
         data = json.load(file)
     with open("blacklistedids.json", "r") as file:
         blcklstdata = json.load(file)
-    with open("alreadyusedids.json", "r") as file:
-        penis = json.load(file)
 
     indx = 0
     for i in data:
+        with open("alreadyusedids.json", "r") as file:
+            penis = json.load(file)
         now = datetime.now()
         current_time = now.strftime("%H:%M")
         indx += 1
@@ -284,13 +284,13 @@ async def on_ready():
     if send_embed == "False":
         await bot.change_presence(status=discord.Status.idle)
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="github.com/hoemotion"))
-        print(f'{Fore.LIGHTGREEN_EX}Logged in as: {Fore.YELLOW}"{bot.user}" {Fore.LIGHTGREEN_EX}| ID: {Fore.YELLOW}"{bot.user.id}"{Fore.LIGHTGREEN_EX}\nConnected with {Fore.YELLOW}{len(bot.guilds)}{Fore.LIGHTGREEN_EX} Guilds and {Fore.YELLOW}{len(bot.user.friends)} {Fore.LIGHTGREEN_EX}Friends')
+        print(f'{Fore.LIGHTGREEN_EX}Logged in as: {Fore.YELLOW}"{bot.user}" {Fore.LIGHTGREEN_EX}| ID: {Fore.YELLOW}"{bot.user}"{Fore.LIGHTGREEN_EX}\nConnected with {Fore.YELLOW}{len(bot.guilds)}{Fore.LIGHTGREEN_EX} Guilds and {Fore.YELLOW}{len(bot.user.friends)} {Fore.LIGHTGREEN_EX}Friends')
         print(f'{Fore.LIGHTYELLOW_EX}[⚡] Started sending DMs to the IDs\n')
         await mass_dm()
     elif send_embed == "True":
         await bot.change_presence(status=discord.Status.idle)
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="github.com/hoemotion"))
-        print(f'{Fore.LIGHTGREEN_EX}Logged in as: {Fore.YELLOW}"Example-User#1234" {Fore.LIGHTGREEN_EX}| ID: {Fore.YELLOW}"253454234534523"{Fore.LIGHTGREEN_EX}\nConnected with {Fore.YELLOW}{len(bot.guilds)}{Fore.LIGHTGREEN_EX} Guilds and {Fore.YELLOW}{len(bot.user.friends)} {Fore.LIGHTGREEN_EX}Friends')
+        print(f'{Fore.LIGHTGREEN_EX}Logged in as: {Fore.YELLOW}"{bot.user}" {Fore.LIGHTGREEN_EX}| ID: {Fore.YELLOW}"{bot.user}"{Fore.LIGHTGREEN_EX}\nConnected with {Fore.YELLOW}{len(bot.guilds)}{Fore.LIGHTGREEN_EX} Guilds and {Fore.YELLOW}{len(bot.user.friends)} {Fore.LIGHTGREEN_EX}Friends')
         print(f'{Fore.LIGHTYELLOW_EX}[⚡] Started sending Embed Messages to the IDs\n')
         await mass_dm_embed()
     else:
